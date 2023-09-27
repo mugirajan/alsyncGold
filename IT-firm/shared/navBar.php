c56b3d1aba72698eeb5e3d5692ae630b759736b0
mugilavathi <mugirajan95@gmail.com>
2023-09-27 15:19:15 +0530

    folder

---
+++ IT-firm/shared/navBar.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>IT-Firm IT Businees HTML-5 Template | Homepage 01</title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+<body>
+	<!-- Main Header -->
+    <header class="main-header header-style-one">
+    	
+		<!-- Header Top -->
+		<div class="header-top">
+			<div class="auto-container">
+				<div class="inner-container d-flex flex-wrap">
+					<!-- Info List -->
+					<ul class="info-list">
+						<li><a href="#"><span class="icon fa-solid fa-phone fa-fw"></span>(678) 345-3456</a></li>
+						<li><a href="#"><span class="icon fa-solid fa-envelope fa-fw"></span>envato@mail.com</a></li>
+						<li><a href="#"><span class="icon fa-solid fa-map fa-fw"></span>380 Albert St, Melbourne, Australia</a></li>
+					</ul>
+					<!-- Social Box -->
+					<ul class="header-social_box">
+						<li><a href="https://www.twitter.com/" class="fa-brands fa-facebook-f fa-fw"></a></li>
+						<li><a href="https://www.facebook.com/" class="fa-brands fa-twitter fa-fw"></a></li>
+						<li><a href="https://dribbble.com/" class="fa-solid fa-dribbble fa-fw"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- End Header Top -->
+		
+        <!-- Header Lower -->
+        <div class="header-lower">
+            
+			<div class="auto-container">
+				<div class="inner-container d-flex justify-content-between align-items-center">
+					
+					<!-- Logo Box -->
+					<div class="logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a></div>
+					
+					<div class="nav-outer d-flex align-items-center">
+						
+						<!-- Main Menu -->
+						<nav class="main-menu show navbar-expand-md">
+							<div class="navbar-header">
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+									<li class="dropdown current"><a href="#">Home</a>
+										<ul>
+											<li><a href="index.html">Homepage One</a></li>
+											<li><a href="index-2.html">Homepage Two</a></li>
+											<li><a href="index-3.html">Homepage Three</a></li>
+											<li><a href="index-4.html">Homepage Four</a></li>
+											<li class="dropdown"><a href="#">Header Styles</a>
+												<ul>
+													<li><a href="index.html">Header Style One</a></li>
+													<li><a href="index-2.html">Header Style Two</a></li>
+													<li><a href="index-3.html">Header Style Three</a></li>
+													<li><a href="index-4.html">Header Style Four</a></li>
+												</ul>
+											</li>
+										</ul>
+									</li>
+									<li><a href="about.html">About Us</a></li>
+									<li class="dropdown"><a href="#">Services</a>
+										<ul>
+											<li><a href="services.html">Services</a></li>
+											<li><a href="service-detail.html">Service Detail</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Project</a>
+										<ul>
+											<li><a href="project.html">project</a></li>
+											<li><a href="project-detail.html">project Detail</a></li>
+										</ul>
+									</li>
+									<li class="dropdown"><a href="#">Blog</a>
+										<ul>
+											<li><a href="blog.html">Our Blog</a></li>
+											<li><a href="blog-detail.html">Blog Detail</a></li>
+											<li><a href="not-found.html">Not Found</a></li>
+										</ul>
+									</li>
+									<li><a href="contact.html">Contact</a></li>
+								</ul>
+							</div>
+							
+						</nav>
+						<!-- Main Menu End-->
+						
+						<!-- Mobile Navigation Toggler -->
+						<div class="mobile-nav-toggler"><span class="icon fa-solid fa-bars fa-fw"></span></div>
+						
+					</div>
+					
+				</div>
+				
+			</div>
+        </div>
+        <!-- End Header Lower -->
+        
+		<!-- Sticky Header  -->
+        <div class="sticky-header">
+            <div class="auto-container">
+				<div class="d-flex justify-content-between align-items-center">
+					<!-- Logo -->
+					<div class="logo">
+						<a href="index.html" title=""><img src="images/logo.png" alt="" title=""></a>
+					</div>
+					
+					<!-- Right Col -->
+					<div class="right-box d-flex align-items-center flex-wrap">
+						<!-- Main Menu -->
+						<nav class="main-menu">
+							<!--Keep This Empty / Menu will come through Javascript-->
+						</nav>
+						<!-- Main Menu End-->
+						
+						<div class="outer-box d-flex align-items-center">
+							
+							<!-- Button Box -->
+							<div class="button-box">
+								<a class="btn-style-three theme-btn btn-item" href="#">
+									<div class="btn-wrap">
+										<span class="text-one">Get a quote <i class="fa-solid fa-arrow-right fa-fw"></i></span>
+										<span class="text-two">Get a quote <i class="fa-solid fa-arrow-right fa-fw"></i></span>
+									</div>
+								</a>
+							</div>
+							
+							<!-- Social Box -->
+							<ul class="header-social_box">
+								<li><a href="https://www.twitter.com/" class="fa-brands fa-facebook-f fa-fw"></a></li>
+								<li><a href="https://www.facebook.com/" class="fa-brands fa-twitter fa-fw"></a></li>
+								<li><a href="https://www.linkedin.com/" class="fa-brands fa-linkedin fa-fw"></a></li>
+								<li><a href="https://instagram.com/" class="fa-solid fa-instagram fa-fw"></a></li>
+							</ul>
+							
+							<!-- Mobile Navigation Toggler -->
+							<div class="mobile-nav-toggler"><span class="icon fa-solid fa-bars fa-fw"></span></div>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+            </div>
+        </div>
+		<!-- End Sticky Menu -->
+		
+		<!-- Mobile Menu  -->
+        <div class="mobile-menu">
+            <div class="menu-backdrop"></div>
+            <div class="close-btn"><span class="icon flaticon-020-x-mark"></span></div>
+            <nav class="menu-box">
+                <div class="nav-logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a></div>
+				<!-- Search -->
+				<div class="search-box">
+					<form method="post" action="contact.html">
+						<div class="form-group">
+							<input type="search" name="search-field" value="" placeholder="SEARCH HERE" required>
+							<button type="submit"><span class="icon flaticon-001-loupe"></span></button>
+						</div>
+					</form>
+				</div>
+                <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+            </nav>
+        </div>
+		<!-- End Mobile Menu -->
+	
+    </header>
+    <!-- End Main Header -->
+	<!-- Scroll To Top -->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-double-up"></span></div>
+
+<script src="js/jquery.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/odometer.js"></script>
+<script src="js/mixitup.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tilt.jquery.min.js"></script>
+<script src="js/magnific-popup.min.js"></script>
+
+<script src="js/script.js"></script>
+</body>
+</html>
